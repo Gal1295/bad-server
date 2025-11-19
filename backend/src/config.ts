e99b0f8f7880608e1e539cd0f4b32bba4d6f6678
Galina Bodunova <galinabodunova@MacBook-Air-Galina.local>
2025-11-19 19:24:28 +0700

    test22

--- backend/src/config.ts
+++ backend/src/config.ts
@@ -1,24 +1,27 @@
 import { CookieOptions } from 'express'
 import ms from 'ms'
 
-export const { PORT = '3000' } = process.env
-export const { DB_ADDRESS = 'mongodb://127.0.0.1:27017/weblarek' } = process.env
-export const { JWT_SECRET = 'JWT_SECRET' } = process.env
+export const PORT = process.env.PORT || '3000'
+export const DB_ADDRESS = process.env.DB_ADDRESS || 'mongodb://127.0.0.1:27017/weblarek'
 export const ACCESS_TOKEN = {
-    secret: process.env.AUTH_ACCESS_TOKEN_SECRET || 'secret-dev',
-    expiry: process.env.AUTH_ACCESS_TOKEN_EXPIRY || '10m',
+  secret: process.env.JWT_SECRET || 'my-super-secret-key-for-tokens-12345',
+  expiry: process.env.AUTH_ACCESS_TOKEN_EXPIRY || '10m',
 }
 export const REFRESH_TOKEN = {
-    secret: process.env.AUTH_REFRESH_TOKEN_SECRET || 'secret-dev',
-    expiry: process.env.AUTH_REFRESH_TOKEN_EXPIRY || '7d',
-    cookie: {
-        name: 'refreshToken',
-        options: {
-            httpOnly: true,
-            sameSite: 'lax',
-            secure: false,
-            maxAge: ms(process.env.AUTH_REFRESH_TOKEN_EXPIRY || '7d'),
-            path: '/',
-        } as CookieOptions,
-    },
+  secret: process.env.JWT_REFRESH_SECRET || 'my-super-secret-key-for-refresh-tokens-12345',
+  expiry: process.env.AUTH_REFRESH_TOKEN_EXPIRY || '7d',
+  cookie: {
+    name: 'refreshToken',
+    options: {
+      httpOnly: true,
+      sameSite: 'lax',
+      secure: false,
+      maxAge: ms(process.env.AUTH_REFRESH_TOKEN_EXPIRY || '7d'),
+      path: '/',
+    } as CookieOptions,
+  },
 }
+
+export const UPLOAD_PATH = process.env.UPLOAD_PATH || 'images'
+export const UPLOAD_PATH_TEMP = process.env.UPLOAD_PATH_TEMP || 'temp'
+export const ORIGIN_ALLOW = process.env.ORIGIN_ALLOW || 'http://localhost:5173'
