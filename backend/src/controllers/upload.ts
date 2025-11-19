2b61ebb45bb505ebf6844205e0329d5e8c489e0e
Galina Bodunova <galinabodunova@MacBook-Air-Galina.local>
2025-11-19 21:20:45 +0700

    test28

--- backend/src/controllers/upload.ts
+++ backend/src/controllers/upload.ts
@@ -1,25 +1,28 @@
 import { NextFunction, Request, Response } from 'express'
 import { constants } from 'http2'
-import BadRequestError from '../errors/bad-request-error'
 import crypto from 'crypto'
 import path from 'path'
+import BadRequestError from '../errors/bad-request-error'
 
-export const uploadFile = async (req: Request, res: Response, next: NextFunction) => {
+export const uploadFile = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
   if (!req.file) {
     return next(new BadRequestError('Файл не загружен'))
   }
 
   try {
-    const fileExtension = path.extname(req.file.originalname)
-    const randomFileName = crypto.randomBytes(16).toString('hex') + fileExtension
+    const ext = path.extname(req.file.originalname)
+    const randomName = crypto.randomBytes(16).toString('hex') + ext
 
     const fileUrl = process.env.UPLOAD_PATH_TEMP
-      ? `/${process.env.UPLOAD_PATH_TEMP}/${randomFileName}`
-      : `/uploads/${randomFileName}`
+      ? `/${process.env.UPLOAD_PATH_TEMP}/${randomName}`
+      : `/uploads/${randomName}`
 
     return res.status(constants.HTTP_STATUS_CREATED).json({
       fileName: fileUrl,
-      originalName: req.file.originalname,
     })
   } catch (error) {
     next(error)
