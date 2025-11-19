ea2f6a4eb6fa5b9bb13bc410e00cb90d3e2054cf
Galina Bodunova <galinabodunova@MacBook-Air-Galina.local>
2025-11-19 18:19:33 +0700

    test17

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,22 +1,21 @@
 import { Router } from 'express'
-import NotFoundError from '../errors/not-found-error'
-
 import authRouter from './auth'
 import customerRouter from './customers'
 import orderRouter from './order'
 import productRouter from './product'
 import uploadRouter from './upload'
 import auth from '../middlewares/auth'
+import NotFoundError from '../errors/not-found-error'
 
 const router = Router()
 
 router.use('/auth', authRouter)
 router.use('/product', productRouter)
-router.use('/order', orderRouter)
-router.use('/customers', customerRouter)
+router.use('/order', auth, orderRouter)
+router.use('/customers', auth, customerRouter)
 router.use('/upload', auth, uploadRouter)
-router.use('*', () => {
-  throw new NotFoundError('Маршрут не найден')
+router.use((req, res, next) => {
+  next(new NotFoundError('Маршрут не найден'))
 })
 
 export default router
