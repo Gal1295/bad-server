05d8ccb27cd2a27a73fa3610d44d04889c311187
Galina Bodunova <galinabodunova@MacBook-Air-Galina.local>
2025-11-20 09:03:44 +0700

    test37/2

--- backend/src/controllers/upload.ts
+++ backend/src/controllers/upload.ts
@@ -1,27 +1,38 @@
 import { NextFunction, Request, Response } from 'express'
 import { constants } from 'http2'
-import BadRequestError from '../errors/bad-request-error'
 import crypto from 'crypto'
 import path from 'path'
+import BadRequestError from '../errors/bad-request-error'
+
+interface MulterRequest extends Request {
+  file: Express.Multer.File
+}
 
 export const uploadFile = async (
-    req: Request,
-    res: Response,
-    next: NextFunction
+  req: Request,
+  res: Response,
+  next: NextFunction
 ) => {
-    if (!req.file) {
-        return next(new BadRequestError('Файл не загружен'))
-    }
+  const file = (req as MulterRequest).file
 
-    try {
-        const ext = path.extname(req.file.originalname)
-        const fileName = crypto.randomBytes(16).toString('hex') + ext
-        const fileUrl = `/uploads/${fileName}`
+  if (!file) {
+    return next(new BadRequestError('Файл не загружен'))
+  }
 
-        return res.status(constants.HTTP_STATUS_CREATED).json({
-            fileName: fileUrl
-        })
-    } catch (error) {
-        next(error)
-    }
+  try {
+    const ext = path.extname(file.originalname).toLowerCase()
+
+    const randomName = crypto.randomBytes(16).toString('hex')
+    const newFileName = randomName + ext
+
+    const fileUrl = process.env.UPLOAD_PATH_TEMP
+      ? `/${process.env.UPLOAD_PATH_TEMP}/${newFileName}`
+      : `/uploads/${newFileName}`
+
+    return res.status(constants.HTTP_STATUS_CREATED).json({
+      fileName: fileUrl,
+    })
+  } catch (error) {
+    next(error)
+  }
 }
