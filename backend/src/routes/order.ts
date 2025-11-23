26ff9b364281c682f307d15a34c28ad0dbff1f8c
Galina Bodunova <galinabodunova@MacBook-Air-Galina.local>
2025-11-23 18:03:10 +0700

    test61

--- backend/src/routes/order.ts
+++ backend/src/routes/order.ts
@@ -1,21 +1,19 @@
-// routes/order.ts
 import { Router } from 'express'
-import { 
-    getOrders, 
-    getOrdersCurrentUser, 
-    getOrderByNumber, 
-    getOrderCurrentUserByNumber, 
-    createOrder, 
-    updateOrder, 
-    deleteOrder 
+import {
+    getOrders,
+    getOrdersCurrentUser,
+    getOrderByNumber,
+    getOrderCurrentUserByNumber,
+    createOrder,
+    updateOrder,
+    deleteOrder
 } from '../controllers/order'
 import { adminGuard } from '../middlewares/auth';
-
 const router = Router()
 
 router.get('/', adminGuard, getOrders)
 router.get('/my', getOrdersCurrentUser)
-router.get('/:orderNumber', getOrderByNumber) 
+router.get('/:orderNumber', getOrderByNumber)
 router.get('/my/:orderNumber', getOrderCurrentUserByNumber)
 router.post('/', createOrder)
 router.patch('/:orderNumber', updateOrder)
