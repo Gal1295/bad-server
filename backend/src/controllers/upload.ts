44b9b31ee616541b6ba9fac66c93dd684501c108
Galina Bodunova <galinabodunova@MacBook-Air-Galina.local>
2025-11-25 16:33:32 +0700

    test86

--- backend/src/controllers/upload.ts
+++ backend/src/controllers/upload.ts
@@ -1,6 +1,8 @@
 import { NextFunction, Request, Response } from 'express'
 import { constants } from 'http2'
+import path from 'path'
 import BadRequestError from '../errors/bad-request-error'
+import movingFile from '../utils/movingFile'
 
 export const uploadFile = async (
     req: Request,
@@ -12,8 +14,16 @@ export const uploadFile = async (
     }
 
     try {
-        const fileName = `/${req.file.filename}`
-        
+        await movingFile(
+            req.file.filename,
+            path.join(__dirname, `../public/${process.env.UPLOAD_PATH_TEMP}`),
+            path.join(__dirname, `../public/${process.env.UPLOAD_PATH}`)
+        )
+
+        const fileName = process.env.UPLOAD_PATH
+            ? `/${process.env.UPLOAD_PATH}/${req.file.filename}`
+            : `/${req.file.filename}`
+            
         return res.status(constants.HTTP_STATUS_CREATED).json({
             fileName,
         })
--- backend/src/utils/movingFile.ts
+++ backend/src/utils/movingFile.ts
@@ -1,19 +1,18 @@
 import { existsSync, rename } from 'fs'
+import { promisify } from 'util'
 import { basename, join } from 'path'
 
-function movingFile(imagePath: string, from: string, to: string) {
+const renameAsync = promisify(rename)
+
+async function movingFile(imagePath: string, from: string, to: string) {
     const fileName = basename(imagePath)
     const imagePathTemp = join(from, fileName)
-    const imagePathPermanent = join(to, fileName)
+    const imagePathPermanent = join(to, fileName)  
     if (!existsSync(imagePathTemp)) {
         throw new Error('Ошибка при сохранении файла')
     }
 
-    rename(imagePathTemp, imagePathPermanent, (err) => {
-        if (err) {
-            throw new Error('Ошибка при сохранении файла')
-        }
-    })
+    await renameAsync(imagePathTemp, imagePathPermanent)
 }
 
 export default movingFile
