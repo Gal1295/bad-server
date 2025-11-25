fc58240a9ce68f4c13a7509a0317f18708acf00a
Galina Bodunova <galinabodunova@MacBook-Air-Galina.local>
2025-11-25 16:07:59 +0700

    test80

--- backend/src/routes/upload.ts
+++ backend/src/routes/upload.ts
@@ -3,6 +3,6 @@ import { uploadFile } from '../controllers/upload'
 import fileMiddleware from '../middlewares/file'
 
 const uploadRouter = Router()
-uploadRouter.post('/', fileMiddleware.single('file'), uploadFile)
+uploadRouter.post('/', fileMiddleware.any(), uploadFile)
 
 export default uploadRouter
