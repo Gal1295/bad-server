b6b6e9b35e5d14735deebe70a7038b3eb2b72831
Galina Bodunova <galinabodunova@MacBook-Air-Galina.local>
2025-11-25 15:18:38 +0700

    test74

--- backend/src/controllers/upload.ts
+++ backend/src/controllers/upload.ts
@@ -1,7 +1,5 @@
 import { NextFunction, Request, Response } from 'express'
 import { constants } from 'http2'
-import path from 'path'
-import { v4 as uuidv4 } from 'uuid'
 import BadRequestError from '../errors/bad-request-error'
 
 export const uploadFile = async (
@@ -13,14 +11,11 @@ export const uploadFile = async (
         return next(new BadRequestError('Файл не загружен'))
     }
     try {
-        const fileExtension = path.extname(req.file.originalname)
-        const uniqueFileName = `${uuidv4()}${fileExtension}`
-        
-        const fileName = `/${uniqueFileName}`
+        const fileName = `/${req.file.filename}`
             
         return res.status(constants.HTTP_STATUS_CREATED).send({
             fileName,
-            fileId: uniqueFileName
+            fileId: req.file.filename
         })
     } catch (error) {
         return next(error)
