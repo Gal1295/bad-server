cadb409d918f0e58f683fa04efdd157e62aba475
Galina Bodunova <galinabodunova@MacBook-Air-Galina.local>
2025-11-25 10:50:40 +0700

    test72

--- backend/src/routes/order.ts
+++ backend/src/routes/order.ts
@@ -9,27 +9,17 @@ import {
     updateOrder,
 } from '../controllers/order'
 import auth, { roleGuardMiddleware } from '../middlewares/auth'
-import { validateOrderBody } from '../middlewares/validations'
+import { validateOrderBody, validatePagination } from '../middlewares/validations'
 import { Role } from '../models/user'
 
 const orderRouter = Router()
 
 orderRouter.post('/', auth, validateOrderBody, createOrder)
-orderRouter.get('/all', auth, getOrders)
-orderRouter.get('/all/me', auth, getOrdersCurrentUser)
-orderRouter.get(
-    '/:orderNumber',
-    auth,
-    getOrderByNumber
-)
+orderRouter.get('/all', auth, roleGuardMiddleware(Role.Admin), validatePagination, getOrders)
+orderRouter.get('/all/me', auth, validatePagination, getOrdersCurrentUser)
+orderRouter.get('/:orderNumber', auth, getOrderByNumber)
 orderRouter.get('/me/:orderNumber', auth, getOrderCurrentUserByNumber)
-orderRouter.patch(
-    '/:orderNumber',
-    auth,
-    roleGuardMiddleware(Role.Admin),
-    updateOrder
-)
-
+orderRouter.patch('/:orderNumber', auth, roleGuardMiddleware(Role.Admin), updateOrder)
 orderRouter.delete('/:id', auth, roleGuardMiddleware(Role.Admin), deleteOrder)
 
 export default orderRouter
